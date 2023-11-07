0e5b663d615de5f58fc6e7adacede7d3a4ee4013
manikOnline <thmanik1241@gmail.com>
2023-11-07 14:19:00 +0600

    Search implement

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.json())
 
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vzgfrzr.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -45,6 +46,16 @@ async function run() {
     const result= await collegeCollection.findOne(query)
     res.send(result)
   })
+  app.get('/collegeSearchByCollegeName/:text',async(req,res)=>{
+    const searchCollege=req.params.text;
+    console.log("We are searching",searchCollege)
+    const result =await collegeCollection.find({
+      $or:[
+        {college_name:{$regex :searchCollege, $options:"i"}}
+      ]
+    }).toArray();
+    res.send(result)
+  })
 
   app.get('/applyForm/:id',async(req,res)=>{
     const id=req.params.id;
@@ -52,6 +63,8 @@ async function run() {
     const result= await collegeCollection.findOne(query)
     res.send(result)
   })
+
+ 
 
   app.post('/userInfo', async(req, res)=>{
     const user=req.body;
@@ -78,6 +91,15 @@ async function run() {
 run().catch(console.dir);
 
 
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+// }));
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 
 
