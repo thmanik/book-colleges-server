798e58b9cd7cb30926b58f9aeaf4e607b7809451
thmanik <thmanik1241@gmail.com>
2025-12-11 21:13:26 +0600

    error fixed

--- index.js
+++ index.js
@@ -79,6 +79,15 @@ async function run() {
     res.send(result)
   })
 
+  // todo
+  // app.delete("mycollege/:id", async(req, res)=>{
+  //   const id=req.params.id;
+  //   console.log("delete from database",id)
+  //   const query={_id:new ObjectId(id)}
+  //   const result= await  usersInfoCollection.deleteOne(query);
+  //   res.send(result)
+  // })
+
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
